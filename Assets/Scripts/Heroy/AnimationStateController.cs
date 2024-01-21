b92e1d4f0fd7e938495013f2a2308f0ac1054e81
Evgeniy <77288589+COBRA445@users.noreply.github.com>
2024-01-21 20:20:34 +0300

    Animation

--- Assets/Scripts/Heroy/AnimationStateController.cs
+++ Assets/Scripts/Heroy/AnimationStateController.cs
@@ -10,6 +10,15 @@ public class AnimationStateController : MonoBehaviour
     private bool isRun;
     private bool isJump;
 
+    private float velocity = 0.0f;
+    [SerializeField]
+    private float acceleration = 0.1f;
+    [SerializeField]
+    private float deceleration = 0.5f;
+    private float _transitions = 0.5f;
+
+    
+
     private void Start()
     {
         animator = GetComponent<Animator>();
@@ -23,15 +32,58 @@ public class AnimationStateController : MonoBehaviour
         isJump = Input.GetKey(KeyCode.Space);
         //Debug.Log("isWalk = " + isWalk);
         //Debug.Log("isRun = " + isRun);
-        Debug.Log("isJump = " + isJump);
+        //Debug.Log("isJump = " + isJump);
+        //if (isWalk) {
+        //    if (isRun) {
+        //        animator.SetBool("isRun", true);
+        //    } else {
+        //        animator.SetBool("isRun", false);
+        //        animator.SetBool("isWalking", true);
+        //    }
+        //}
+
+        //if (isJump) {
+        //    animator.SetBool("isJump", true);
+        //} else {
+        //    animator.SetBool("isJump", false);
+        //}
+
+        //if (!isWalk) {
+        //    animator.SetBool("isWalking", false);
+        //    animator.SetBool("isRun", false);
+        //}
+
+
+        if (isWalk) {
+           velocity += Time.deltaTime * acceleration;
+        }
+
         if (isWalk) {
             if (isRun) {
-                animator.SetBool("isRun", true);
+                if (velocity > 1f){
+                    velocity = 1f;
+                } else {
+                    velocity += Time.deltaTime * acceleration;
+                }
+            }
+            if (!isRun && velocity >= _transitions) {
+                velocity -= Time.deltaTime * deceleration;
             } else {
-                animator.SetBool("isRun", false);
-                animator.SetBool("isWalking", true);
+                velocity += Time.deltaTime * acceleration;
             }
         }
+
+        if (!isRun && velocity > _transitions) {
+            velocity -= Time.deltaTime * deceleration;
+        }
+
+        if (!isWalk && velocity > 0) {            
+            velocity -= Time.deltaTime * deceleration;
+        }
+
+        animator.SetFloat("Velocity", velocity);
+
+
 
         if (isJump) {
             animator.SetBool("isJump", true);
@@ -39,9 +91,5 @@ public class AnimationStateController : MonoBehaviour
             animator.SetBool("isJump", false);
         }
 
-        if (!isWalk) {
-            animator.SetBool("isWalking", false);
-            animator.SetBool("isRun", false);
-        }
     }
 }
